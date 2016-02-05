525331c949d2e7fceddbbafc8b9c03bb300c94a7
Pierre Besson <pierre.besson7@gmail.com>
2016-02-05 13:27:15 +0100

    [structure]

--- src/example/index.js
+++ src/example/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MyComponent from '../src';
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import MyComponent from '../';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import 'material-design-lite/material.css';
 import 'material-design-lite/material.min';
 
@@ -10,13 +10,14 @@ import 'material-design-lite/material.min';
 document.addEventListener('DOMContentLoaded', (event) => {
 
     const rootElement = document.querySelector(`.${__ANCHOR_CLASS__}`);
+    const PAGE_TITLE = 'Great example page';
     // The child must be wrapped in a function
     // to work around an issue in React 0.13.
     ReactDOM.render(
             <div className='mdl-layout  mdl-layout--fixed-header'>
             <header className='mdl-layout__header'>
             <div className='mdl-layout__header-row'>
-              <span className='mdl-layout-title'>Title</span>
+              <span className='mdl-layout-title'>{PAGE_TITLE}</span>
               <div className='mdl-layout-spacer'></div>
               <nav className='mdl-navigation mdl-layout--large-screen-only'>
                 <a className='mdl-navigation__link' href=''>Link</a>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React , { Component , PropTypes }from 'react';
+import React , { Component , PropTypes } from 'react';
 export default function Data(){
-  return <div>Data</div>
+  return <div>Your Component</div>
 }
